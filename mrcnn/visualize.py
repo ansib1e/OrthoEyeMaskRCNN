18383db7cdcd6c98d736bdb93693bce00fb95fc9
Hubert Yoo <58751186+ansib1e@users.noreply.github.com>
2021-12-03 03:17:04 -0500

    Add files via upload

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -190,8 +190,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     output_mask = masked_image.astype(np.uint8)
     if not (save_fig_path is None):
         plt.savefig(save_fig_path, bbox_inches="tight")
-    #if auto_show:
-        #plt.show()
+    if auto_show:
+        plt.show()
     return output_mask
 
 def display_differences(image,
